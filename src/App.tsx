56fa5e5c96ee879557068f1aacc54e01f5ed60eb
Miran Korkmaz <miran.korkmaz@booli.se>
2024-07-25 12:11:58 +0200

    Try downloadLink for downloading pdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { PDFViewer } from "@react-pdf/renderer";
+import { PDFDownloadLink } from "@react-pdf/renderer";
 import "./App.css";
 import MyDocument from "./common/Document";
 import LandingPage from "./common/LandingPage";
 import LanguagePicker from "./common/LanguagePicker";
+import { useTranslation } from "react-i18next";
 
 const App = () => {
+  const { t } = useTranslation();
   return (
     <>
       <LanguagePicker />
       <LandingPage />
-      <PDFViewer>
-        <MyDocument />
-      </PDFViewer>
+      <PDFDownloadLink document={<MyDocument />} fileName="somename.pdf">
+        {({ loading }) =>
+          loading ? t("Download.loading") : t("Download.loadingDone")
+        }
+      </PDFDownloadLink>
     </>
   );
 };
